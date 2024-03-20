720f6c7d97b95dedfb32878aaf59a3bfe8ab9493
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2024-03-20 10:05:01 +0300

    Init

--- Scripts/Zone/DeleteZone.cs
+++ Scripts/Zone/DeleteZone.cs
@@ -4,16 +4,10 @@ public class DeleteZone : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.TryGetComponent(out Enemy enemy))
-        {
-            if (enemy.gameObject.activeSelf == true)
-                enemy.gameObject.SetActive(false);
-        }
+        if (collision.TryGetComponent(out Enemy enemy) && enemy.gameObject.activeSelf == true)
+            enemy.gameObject.SetActive(false);
 
-        if (collision.TryGetComponent(out Bullet bullet))
-        {
-            if (bullet.gameObject.activeSelf == true)
-                bullet.gameObject.SetActive(false);
-        }
+        if (collision.TryGetComponent(out Bullet bullet) && bullet.gameObject.activeSelf == true)
+            bullet.gameObject.SetActive(false);
     }
 }
